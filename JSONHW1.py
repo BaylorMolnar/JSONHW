3f0404867052e5625d451188126f1123a0500d2b
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-02-25 11:10:44 -0600

    Initial Commit- 1st Finish

---
+++ JSONHW1.py
@@ -0,0 +1,47 @@
+import json
+
+infile = open("US_fires_9_1.json", "r")
+
+
+# json load converts data
+fire_data = json.load(infile)
+
+# list_of_fires = fire_data["features"]
+bright, lons, lats = [], [], []
+
+for fire in fire_data:
+    bright_level = fire["brightness"]
+    lon = fire["longitude"]
+    lat = fire["latitude"]
+
+    if int(bright_level) >= 450:
+        bright.append(bright_level)
+        lons.append(lon)
+        lats.append(lat)
+
+# print(mags[:10])
+# print(lons[:10])
+# print(lats[:10])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+#data = [Scattergeo(lon=lons, lat=lats)]
+data = [
+    {
+        "type": "scattergeo",
+        "lon": lons,
+        "lat": lats,
+        "marker": {
+            "color": bright,
+            "colorscale": "viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+
+my_layout = Layout(title="US Fires - 9/1/20-9/13/20")
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="California_Fires_First_Half.html")
